2099108b721d359f0dbe5e103f9bcbafe7222923
Manabu Sakurai <27march.2725@gmail.com>
2020-05-04 01:01:30 +0900

    Create Main.vue

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <Header />
     <div class="app-container">
     <SideMenu />
+    <Main />
     </div>
   </div>
 </template>
@@ -11,17 +12,20 @@
 import 'normalize.css';
 import Header from './components/Header';
 import SideMenu from './components/SideMenu';
+import Main from './components/Main';
 
 export default {
   components:{
     Header,
     SideMenu,
+    Main
   }
 }
 </script>
 
 <style>
 .app-container {
+  display: flex;
   padding-top: 80px;
 }
 </style>
